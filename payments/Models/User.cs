545cfd47920167148714badc3823b6473c914cf1
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2023-05-12 14:10:17 +0900

    payments: user model

---
+++ payments/Models/User.cs
@@ -0,0 +1,10 @@
+namespace Api.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public string FullName { get; set; } = string.Empty;
+    public bool IsActive { get; set; }
+    public string HashedPassword { get; set; } = string.Empty;
+}
