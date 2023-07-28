9e87f57b699ce68da0ea526c659e078b144a8ceb
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2023-07-28 13:15:06 +0900

    payments: tag model

---
+++ payments/Models/Tag.cs
@@ -0,0 +1,8 @@
+namespace Api.Models;
+
+public class Tag
+{
+    public int Id { get; set; }
+    public string Label { get; set; } = string.Empty;
+    public string Color { get; set; } = string.Empty;
+}
